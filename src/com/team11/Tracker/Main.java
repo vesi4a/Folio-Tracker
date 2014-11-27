536eb4a677c1a82cb0f658df5ee6a20bb5059d0a
Nightpaws <nightpaws@users.noreply.github.com>
2014-11-27 15:58:25 +0000

    Added sample menu code

--- src/com/team11/Tracker/Main.java
+++ src/com/team11/Tracker/Main.java
@@ -3,8 +3,13 @@ package com.team11.Tracker;
 public class Main {
 
 	public static void main(String[] args) {
-		// For Starting up the interface Class
-
+        //Schedule a job for the event-dispatching thread:
+        //creating and showing this application's GUI.
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                Interface.show();
+            }
+        });
 	}
 
 }
