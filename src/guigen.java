6aa48c579b4afc390c90cf00e0039059ead62573
ZlatanIbrahimovic <liamdiamond@hotmail.co.uk>
2014-12-08 01:50:57 +0000

    Added a box to edit the stock

--- src/guigen.java
+++ src/guigen.java
@@ -1,19 +1,11 @@
-import java.awt.BorderLayout;
-import java.awt.Container;
-import java.awt.Dimension;
+
 import java.awt.EventQueue;
-import java.awt.Rectangle;
 
 import javax.swing.JFrame;
 import javax.swing.JMenuBar;
 import javax.swing.JMenu;
 import javax.swing.JMenuItem;
-
-import java.awt.FlowLayout;
-
 import javax.swing.BoxLayout;
-import javax.swing.GroupLayout;
-import javax.swing.GroupLayout.Alignment;
 import javax.swing.JFileChooser;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
@@ -29,8 +21,7 @@ import java.io.File;
 
 import javax.swing.JTable;
 import javax.swing.JScrollPane;
-import javax.swing.table.DefaultTableModel;
-import javax.swing.table.TableColumn;
+
 
 
 public class guigen {
@@ -40,12 +31,14 @@ public class guigen {
 	private JTextField textField_1;
 	private static JTabbedPane tabbedPane;
 	private JScrollPane scrollPane;
+//	private static JScrollPane scrollPane2;
 
 	/**
 	 * Launch the application.
 	 */
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
+			@SuppressWarnings("static-access")
 			public void run() {
 				try {
 					guigen window = new guigen();
@@ -85,25 +78,25 @@ public class guigen {
 		createTab("Portfolio1");
 
 		// Labels and text fields
-		JLabel lblAddStock = new JLabel("Add Stock-");
+		JLabel lblAddStock = new JLabel("Add Stock:");
 		lblAddStock.setBounds(16, 485, 84, 16);
 		frame.getContentPane().add(lblAddStock);
 
 		JLabel lblTickerSymbol = new JLabel("Ticker Symbol");
-		lblTickerSymbol.setBounds(125, 485, 89, 16);
+		lblTickerSymbol.setBounds(112, 485, 89, 16);
 		frame.getContentPane().add(lblTickerSymbol);
 
 		textField = new JTextField();
-		textField.setBounds(230, 479, 84, 28);
+		textField.setBounds(213, 479, 84, 28);
 		frame.getContentPane().add(textField);
 		textField.setColumns(10);
 
 		JLabel lblAmount = new JLabel("Amount");
-		lblAmount.setBounds(326, 485, 55, 16);
+		lblAmount.setBounds(309, 485, 55, 16);
 		frame.getContentPane().add(lblAmount);
 
 		textField_1 = new JTextField();
-		textField_1.setBounds(377, 479, 72, 28);
+		textField_1.setBounds(363, 479, 72, 28);
 		frame.getContentPane().add(textField_1);
 		textField_1.setColumns(10);
 
@@ -113,15 +106,15 @@ public class guigen {
 		frame.getContentPane().add(btnAdd);
 
 		JButton btnDel = new JButton("Delete");
-		btnDel.setBounds(545, 520, 117, 29);
+		btnDel.setBounds(545, 521, 117, 29);
 		frame.getContentPane().add(btnDel);
 
 		JButton btnHist = new JButton("History");
-		btnHist.setBounds(670, 520, 117, 29);
+		btnHist.setBounds(247, 529, 117, 29);
 		frame.getContentPane().add(btnHist);
 
 		JLabel lblnull_1 = new JLabel("(null)");
-		lblnull_1.setBounds(708, 482, 61, 22);
+		lblnull_1.setBounds(694, 487, 61, 22);
 		frame.getContentPane().add(lblnull_1);
 
 		JLabel lblTotalValue = new JLabel("Total value of portfolio:");
@@ -131,9 +124,15 @@ public class guigen {
 		JLabel lblnull = new JLabel("(null)");
 		lblnull.setBounds(177, 534, 61, 16);
 		frame.getContentPane().add(lblnull);
+		
+		JButton btnNewButton = new JButton("Close");
+		btnNewButton.setBounds(662, 521, 117, 29);
+		frame.getContentPane().add(btnNewButton);
 
-		confirmDelete();
-		createNewPortfolio();
+		//confirmDelete();
+		//createNewPortfolio();
+		//stockAddingError();
+		//editStock();
 
 	}
 
@@ -154,7 +153,115 @@ public class guigen {
 		tabbedPane.addTab(PortfolioName, null, scrollPane, null);
 		
 	}
-
+	
+	private static void editStock(){
+//		// Create columns names
+//				String columnNames[] = { "", ""};
+//
+//				// Create some data
+//				String dataValues[][] = { {"TST1", "Test Stock 3"}, {"Current Value: ", "3.0"}, {"Daily Change: ", "0.0"}, 
+//						{"Number of Shares", "300"}, {"Initial value", "1.0"}, {"Total gain", "-127.0"} };
+//
+//				// Create a new table instance
+//				JTable table = new JTable(dataValues, columnNames);
+//				//table.setFillsViewportHeight(true);
+//				
+//				// Add the table to a scrolling pane
+//				scrollPane2 = new JScrollPane(table);
+//				scrollPane2.setMaximumSize(new Dimension(100,100));
+//				JOptionPane.showMessageDialog(null, scrollPane2, "dialog test with textarea",  
+//				                                       JOptionPane.INFORMATION_MESSAGE);
+		JFrame frame2;
+		JTextField textField1;
+		JTextField textField2;
+		
+		frame2 = new JFrame("Edit stock");
+		frame2.setBounds(100, 100, 450, 300);
+		frame2.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame2.getContentPane().setLayout(null);
+		frame2.setVisible(true);
+		
+		JPanel panel = new JPanel();
+		panel.setBounds(6, 6, 364, 221);
+		frame2.getContentPane().add(panel);
+		panel.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("Editing Portfolio:");
+		lblNewLabel.setBounds(6, 6, 115, 16);
+		panel.add(lblNewLabel);
+		
+		JLabel lblNewLabel_1 = new JLabel("TestPortfolio 1");
+		lblNewLabel_1.setBounds(185, 6, 121, 16);
+		panel.add(lblNewLabel_1);
+		
+		JLabel lblTst = new JLabel("TST3");
+		lblTst.setBounds(6, 32, 115, 16);
+		panel.add(lblTst);
+		
+		JLabel lblTes = new JLabel("Test Stock 3");
+		lblTes.setBounds(185, 32, 111, 16);
+		panel.add(lblTes);
+		
+		JLabel lblCurrentValue = new JLabel("Current Value:");
+		lblCurrentValue.setBounds(6, 60, 115, 16);
+		panel.add(lblCurrentValue);
+		
+		JLabel lblNewLabel_2 = new JLabel("3.0");
+		lblNewLabel_2.setBounds(133, 60, 51, 16);
+		panel.add(lblNewLabel_2);
+		
+		JLabel lblNewLabel_3 = new JLabel("Daily Change:");
+		lblNewLabel_3.setBounds(185, 60, 98, 16);
+		panel.add(lblNewLabel_3);
+		
+		JLabel lblNewLabel_4 = new JLabel("0.0");
+		lblNewLabel_4.setBounds(295, 60, 61, 16);
+		panel.add(lblNewLabel_4);
+		
+		JLabel lblNumberOfShares = new JLabel("Number Of Shares:");
+		lblNumberOfShares.setBounds(6, 88, 121, 16);
+		panel.add(lblNumberOfShares);
+		
+		textField1 = new JTextField();
+		textField1.setBounds(185, 82, 134, 28);
+		panel.add(textField1);
+		textField1.setColumns(10);
+		
+		JLabel lblInitialValue = new JLabel("Initial Value:");
+		lblInitialValue.setBounds(6, 117, 121, 16);
+		panel.add(lblInitialValue);
+		
+		textField2 = new JTextField();
+		textField2.setBounds(185, 111, 134, 28);
+		panel.add(textField2);
+		textField2.setColumns(10);
+		
+		JLabel lblTotalGain = new JLabel("Total Gain:");
+		lblTotalGain.setBounds(6, 145, 115, 16);
+		panel.add(lblTotalGain);
+		
+		JLabel lblNewLabel_5 = new JLabel("-297.0");
+		lblNewLabel_5.setBounds(133, 145, 80, 16);
+		panel.add(lblNewLabel_5);
+		
+		JButton btnSave = new JButton("Save");
+		btnSave.setBounds(6, 175, 117, 29);
+		panel.add(btnSave);
+		
+		JButton btnCancel = new JButton("Cancel");
+		btnCancel.setBounds(128, 175, 117, 29);
+		panel.add(btnCancel);
+		
+		JButton btnDelete = new JButton("Delete");
+		btnDelete.setBounds(247, 175, 117, 29);
+		panel.add(btnDelete);
+				
+	}
+	
+	private static void stockAddingError(){
+		JOptionPane.showMessageDialog(frame, "Number of shares must be a positive number",
+		        "Error Adding Stock", JOptionPane.INFORMATION_MESSAGE);
+	}
 	private static void confirmDelete() {
 		int dialogButton = JOptionPane.YES_NO_OPTION;
 		int dialogResult = JOptionPane.showConfirmDialog(frame,
