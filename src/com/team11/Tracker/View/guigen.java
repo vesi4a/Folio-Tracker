46ab20579002a155d74a536ba2bd4e75ab790cc4
Nightpaws <nightpaws@users.noreply.github.com>
2014-12-08 11:18:57 +0000

    Centred Window
    Changed positioning code to centre the window and maintain the 800x600
    as previous code placed in top right corner

--- src/com/team11/Tracker/View/guigen.java
+++ src/com/team11/Tracker/View/guigen.java
@@ -64,7 +64,9 @@ public class guigen {
 	 */
 	private void initialize() {
 		frame = new JFrame("Folio Tracker");
-		frame.setBounds(100, 100, 800, 600);
+//		frame.setBounds(100, 100, 800, 600);
+		frame.setSize(800, 600);
+		frame.setLocationRelativeTo(null);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
 		JMenuBar mainMnu = menuSetup();
