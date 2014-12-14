ff22f893237c618eb863fe0edf9fdcea1052fe2f
Cameron Deans <CDeansy@gmail.com>
2014-12-14 21:04:57 +0000

    Liams popup for creating a new portfolio

--- src/com/team11/Tracker/Controller/MenuBarListener.java
+++ src/com/team11/Tracker/Controller/MenuBarListener.java
@@ -4,6 +4,8 @@ import com.team11.Tracker.Model.Portfolio;
 import com.team11.Tracker.View.View;
 
 
+import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -26,8 +28,46 @@ public class MenuBarListener implements ActionListener {
     public void actionPerformed(ActionEvent e) {
         if (e.getActionCommand().equals("NewFolio")) {
             System.out.println("Add folio menu item pressed");
-            cntrl.getPortfolioHolder().addPortfolio(new Portfolio("TEST NAME"));
-            view.createTab("TEST NAME");
+
+            // TODO: This should be moved to a function in the view.
+
+            while (true){
+                JTextField portFolioNameEntry = new JTextField(20);
+
+                JPanel newPortFolioPanel = new JPanel();
+
+                JLabel enterPortFolioNameLabel = new JLabel("Enter PortFolio Name:");
+
+                newPortFolioPanel.setLayout(new BoxLayout(newPortFolioPanel,
+                        BoxLayout.Y_AXIS));
+
+                portFolioNameEntry.setMaximumSize(new Dimension(1500, 20));
+
+                newPortFolioPanel.add(enterPortFolioNameLabel);
+                newPortFolioPanel.add(portFolioNameEntry);
+
+                int result = JOptionPane.showConfirmDialog(null, newPortFolioPanel,
+                        "Create New PortFolio", JOptionPane.OK_CANCEL_OPTION);
+
+                String projname = (portFolioNameEntry.getText());
+
+                if (result == JOptionPane.CANCEL_OPTION){
+                    break;
+                }
+                if (projname.equals("") || projname.equals(null)){
+                    // TODO: Add check for a unique Portfolio name
+                    JLabel enterFilenameLabel = new JLabel("File Name required.");
+                    newPortFolioPanel.add(enterFilenameLabel);
+
+                }
+                else{
+                    // Creates the tab and portfolio
+                    view.createTab(projname);
+                    cntrl.getPortfolioHolder().addPortfolio(new Portfolio(projname));
+                    break;
+                }
+            }
+
         }
         else if (e.getActionCommand().equals("OpenFolio")) {
             System.out.println("Open folio menu item pressed");
