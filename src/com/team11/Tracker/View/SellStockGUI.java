90c200c8d6c8a2d7f72d4c8195316f673ad81f70
Nightpaws <nightpaws@users.noreply.github.com>
2014-12-15 23:48:09 +0000

    Fixed Errors in SellStockGUI
    GUI I had built required external dependencies which isn’t acceptable
    in the target MVC we are aiming for. Rebuilt (better) with simpler
    structure.

--- src/com/team11/Tracker/View/SellStockGUI.java
+++ src/com/team11/Tracker/View/SellStockGUI.java
@@ -7,14 +7,18 @@ import javax.swing.JFrame;
 import java.awt.Window.Type;
 import java.util.Observable;
 import java.util.Observer;
-import com.jgoodies.forms.layout.FormLayout;
-import com.jgoodies.forms.layout.ColumnSpec;
-import com.jgoodies.forms.layout.RowSpec;
-import com.jgoodies.forms.factories.FormFactory;
+//import com.jgoodies.forms.layout.FormLayout;
+//import com.jgoodies.forms.layout.ColumnSpec;
+//import com.jgoodies.forms.layout.RowSpec;
+//import com.jgoodies.forms.factories.FormFactory;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
 import javax.swing.JButton;
 import java.awt.Dimension;
+import java.awt.BorderLayout;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import java.awt.GridLayout;
 
 public class SellStockGUI implements Observer {
 
@@ -39,49 +43,32 @@ public class SellStockGUI implements Observer {
 		frmSellStock.setType(Type.UTILITY);
 		frmSellStock.setTitle("Sell Stock");
 		frmSellStock.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		frmSellStock.getContentPane().setLayout(
-				new FormLayout(new ColumnSpec[] {
-						FormFactory.RELATED_GAP_COLSPEC,
-						FormFactory.DEFAULT_COLSPEC,
-						FormFactory.RELATED_GAP_COLSPEC,
-						ColumnSpec.decode("default:grow"), }, new RowSpec[] {
-						FormFactory.RELATED_GAP_ROWSPEC,
-						FormFactory.DEFAULT_ROWSPEC,
-						FormFactory.RELATED_GAP_ROWSPEC,
-						FormFactory.DEFAULT_ROWSPEC,
-						FormFactory.RELATED_GAP_ROWSPEC,
-						FormFactory.DEFAULT_ROWSPEC,
-						FormFactory.RELATED_GAP_ROWSPEC,
-						FormFactory.DEFAULT_ROWSPEC,
-						FormFactory.RELATED_GAP_ROWSPEC,
-						FormFactory.DEFAULT_ROWSPEC, }));
-
+		frmSellStock.getContentPane().setLayout(new GridLayout(4, 2, 5, 5));
+	
 		JLabel lblStockSymbol = new JLabel("Stock Symbol:");
-		frmSellStock.getContentPane().add(lblStockSymbol, "2, 2");
+		frmSellStock.getContentPane().add(lblStockSymbol);
 
 		JLabel lblSymbol = new JLabel("SMPL");
-		frmSellStock.getContentPane().add(lblSymbol, "4, 2");
+		frmSellStock.getContentPane().add(lblSymbol);
 
 		JLabel lblCurrentValue = new JLabel("Current Value:");
-		frmSellStock.getContentPane().add(lblCurrentValue, "2, 4");
+		frmSellStock.getContentPane().add(lblCurrentValue);
 
 		JLabel lblValue = new JLabel("0.00");
-		frmSellStock.getContentPane().add(lblValue, "4, 4");
+		frmSellStock.getContentPane().add(lblValue);
 
 		JLabel lblSellAmount = new JLabel("Amount to Sell:");
-		frmSellStock.getContentPane().add(lblSellAmount, "2, 6");
+		frmSellStock.getContentPane().add(lblSellAmount);
 
 		txtSellAmount = new JTextField();
-		txtSellAmount.setPreferredSize(new Dimension(36, 28));
-		frmSellStock.getContentPane().add(txtSellAmount, "4, 6, fill, default");
+		frmSellStock.getContentPane().add(txtSellAmount, "fill, default");
 		txtSellAmount.setColumns(10);
 
 		JButton btnSell = new JButton("Sell Shares");
-		btnSell.setPreferredSize(new Dimension(86, 29));
-		frmSellStock.getContentPane().add(btnSell, "2, 8");
+		frmSellStock.getContentPane().add(btnSell);
 
 		JButton btnCancel = new JButton("Cancel");
-		frmSellStock.getContentPane().add(btnCancel, "4, 8");
+		frmSellStock.getContentPane().add(btnCancel);
 
 		frmSellStock.pack();
 		frmSellStock.setLocationRelativeTo(null);
