aaca84a054667c2989d0e16685e33630ef1b7da4
Nightpaws <nightpaws@users.noreply.github.com>
2014-12-16 21:13:51 +0000

    Added exception handling for UI
    Should no longer throw exceptions when the UI reaches exception states.

--- src/com/team11/Tracker/Controller/MenuBarListener.java
+++ src/com/team11/Tracker/Controller/MenuBarListener.java
@@ -9,107 +9,111 @@ import java.awt.event.ActionListener;
 import java.io.File;
 import java.io.FileNotFoundException;
 
-
 public class MenuBarListener implements ActionListener {
 
-    private MainGUI view;
-    private IPortfolioHolder holder;
+	private MainGUI view;
+	private IPortfolioHolder holder;
 
-    public MenuBarListener(MainGUI view, IPortfolioHolder holder) {
-        this.view = view;
-        this.holder = holder;
-    }
+	public MenuBarListener(MainGUI view, IPortfolioHolder holder) {
+		this.view = view;
+		this.holder = holder;
+	}
 
-    @Override
-    public void actionPerformed(ActionEvent e) {
-        if (e.getActionCommand().equals("NewFolio")) {
-            System.out.println("Add folio menu item pressed");
-            view.showNewFolioAlert();
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if (e.getActionCommand().equals("NewFolio")) {
+			System.out.println("Add folio menu item pressed");
+			view.showNewFolioAlert();
 
-            view.getTpPortfolioView().setSelectedIndex(
-                    view.getTpPortfolioView().getTabCount() - 1);
+			view.getTpPortfolioView().setSelectedIndex(
+					view.getTpPortfolioView().getTabCount() - 1);
 
-        } else if (e.getActionCommand().equals("OpenFolio")) {
-            System.out.println("Open folio menu item pressed");
-            File f = view.showLoadFolioAlert();
-            try {
-                // Create the new portfolio
-                Portfolio pf = holder.loadFolio(f, view);
+		} else if (e.getActionCommand().equals("OpenFolio")) {
+			System.out.println("Open folio menu item pressed");
+			File f = view.showLoadFolioAlert();
+			try {
+				// Create the new portfolio
+				Portfolio pf = holder.loadFolio(f, view);
 
-                // Add the portfolio to the holder
-                holder.addPortfolio(pf);
-                // Create a new tab for the portfolio
-                view.createTab(pf.getPortfolioName());
-                // Set the new tab as the active tab so that we can add a share to
-                // its table
-                view.getTpPortfolioView().setSelectedIndex(
-                        view.getTpPortfolioView().getTabCount() - 1);
+				// Add the portfolio to the holder
+				holder.addPortfolio(pf);
+				// Create a new tab for the portfolio
+				view.createTab(pf.getPortfolioName());
+				// Set the new tab as the active tab so that we can add a share
+				// to
+				// its table
+				view.getTpPortfolioView().setSelectedIndex(
+						view.getTpPortfolioView().getTabCount() - 1);
 
+				pf.updateAllShares();
 
-                pf.updateAllShares();
+			} catch (FileNotFoundException e1) {
+				// Error loading the folio
+				e1.printStackTrace();
+			}
 
-            } catch (FileNotFoundException e1) {
-                // Error loading the folio
-                e1.printStackTrace();
-            }
+		} else if (e.getActionCommand().equals("SaveFolio")) {
+			if (!holder.getPortfolios().isEmpty()) {
 
-        } else if (e.getActionCommand().equals("SaveFolio")) {
-            if(!holder.getPortfolios().isEmpty()) {
+				System.out.println("Save folio menu item pressed");
+				Portfolio currentPortfolio = holder.getPortfolios().get(
+						view.getTpPortfolioView().getSelectedIndex());
+				holder.saveFolio(currentPortfolio,
+						currentPortfolio.getPortfolioName());
+			} else {
+				view.showNoOpenFolioAlert();
+			}
 
-                System.out.println("Save folio menu item pressed");
-                Portfolio currentPortfolio = holder.getPortfolios().get(view.getTpPortfolioView().getSelectedIndex());
-                holder.saveFolio(currentPortfolio, currentPortfolio.getPortfolioName());
-            }
-            else {
-                view.showNoOpenFolioAlert();
-            }
+		} else if (e.getActionCommand().equals("RefreshFolio")) {
+			System.out.println("Refresh item pressed");
+			if (!holder.getPortfolios().isEmpty()) {
+				holder.getPortfolios()
+						.get(view.getTpPortfolioView().getSelectedIndex())
+						.updateAllShares();
+			} else {
+				view.showNoOpenFolioAlert();
+			}
+		} else if (e.getActionCommand().equals("ViewHistory")) {
 
-        } else if (e.getActionCommand().equals("RefreshFolio")) {
-            System.out.println("Refresh item pressed");
-            if (!holder.getPortfolios().isEmpty()) {
-                holder.getPortfolios()
-                        .get(view.getTpPortfolioView().getSelectedIndex())
-                        .updateAllShares();
-            }
-            else {
-                view.showNoOpenFolioAlert();
-            }
-        } else if (e.getActionCommand().equals("ViewHistory")) {
+			System.out.println("View History menu item pressed");
 
-            System.out.println("View History menu item pressed");
+		} else if (e.getActionCommand().equals("CloseFolio")) {
 
-        }  else if (e.getActionCommand().equals("CloseFolio")) {
+			if (!holder.getPortfolios().isEmpty()) {
+				System.out.println("Close folio menu item pressed");
+				int currentPaneNumber = TabChangeListener.paneNo;
+				if (view.getTpPortfolioView().getTabCount() > 1) {
 
-            System.out.println("Close folio menu item pressed");
-            int currentPaneNumber = TabChangeListener.paneNo;
-            if (view.getTpPortfolioView().getTabCount() > 1) {
+					int result = view.showSaveBeforeCloseAlert();
+					if (result == 1) { // yes
+						System.out.println("Saving Folio...");
+						Portfolio currentPortfolio = holder.getPortfolios()
+								.get(view.getTpPortfolioView()
+										.getSelectedIndex());
+						holder.saveFolio(currentPortfolio,
+								currentPortfolio.getPortfolioName());
+						System.out.println("Folio saved.");
+						holder.removeFolio(currentPortfolio);
+						view.getTpPortfolioView().remove(currentPaneNumber);
+						return;
+					} else if (result == 2) { // no
+						view.getTpPortfolioView().remove(currentPaneNumber);
+						return;
+					}
+				} else {
+					System.out.println("Cannot close the only portfolio");
+					view.showOnlyPortfolioAlert();
+				}
 
-                int result = view.showSaveBeforeCloseAlert();
-                if (result == 1) { // yes
-                    System.out.println("Saving Folio...");
-                    Portfolio currentPortfolio = holder.getPortfolios().get(
-                            view.getTpPortfolioView().getSelectedIndex());
-                    holder.saveFolio(currentPortfolio,
-                            currentPortfolio.getPortfolioName());
-                    System.out.println("Folio saved.");
-                    holder.removeFolio(currentPortfolio);
-                    view.getTpPortfolioView().remove(currentPaneNumber);
-                    return;
-                } else if (result == 2) { // no
-                    view.getTpPortfolioView().remove(currentPaneNumber);
-                    return;
-                }
-            }
-            else {
-                // Can't currently close the only folio
-            }
+			} else {
+				view.showNoOpenFolioAlert();
+			}
 
-        } else if (e.getActionCommand().equals("Exit")) {
-            System.out.println("Exit menu item pressed");
-            // Add a popup to confirm?
-            view.showCloseAlert();
-        }
+		} else if (e.getActionCommand().equals("Exit")) {
+			System.out.println("Exit menu item pressed");
+			// Add a popup to confirm?
+			view.showCloseAlert();
+		}
 
-
-    }
+	}
 }
